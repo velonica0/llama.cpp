8a0f042c9af4892c85b59c27840b93b6890092d3
marty1885 <marty188586@gmail.com>
2024-11-11 12:24:16 +0800

    add missing func

--- ggml/src/ggml-metalium.cpp
+++ ggml/src/ggml-metalium.cpp
@@ -2126,8 +2126,8 @@ static bool ggml_backend_metalium_device_supports_op_internal(ggml_backend_dev_t
             return tensor_supported(src1) && ggml_backend_metalium_can_get_row(op);
         case GGML_OP_CONCAT:
             return tensor_supported(src1) && ggml_backend_metalium_can_concat(op);
-        // case GGML_OP_SOFT_MAX:   // Not quite as inaccurate to cause incohorence but still not quite right
-        //     return ggml_backend_metalium_can_softmax(op);
+        case GGML_OP_SOFT_MAX:   // Not quite as inaccurate to cause incohorence but still not quite right
+            return ggml_backend_metalium_can_softmax(op) && !g_debug_flags.llm_hacks;
         case GGML_OP_REPEAT:
             return ggml_backend_metalium_can_repeat(op);
         case GGML_OP_OUT_PROD:
